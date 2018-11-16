04bcbb20c143be1cf6714050dc33d77cc7ccd68b
劉哲瑋 <>
2018-11-16 15:06:08 +0800

    創建一個測試webGL的環境

---
+++ Assets/Main/Code/0/Main_0_UICon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_0_UICon : MonoBehaviour {
+	[Serializable]
+	public struct _UI {
+
+	}
+	public _UI UI;
+	
+	void Start () {
+			
+	}
+}
