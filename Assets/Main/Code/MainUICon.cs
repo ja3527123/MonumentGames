026d1d04cb157363e1e2e1af3432e227ad74eb06
劉哲瑋 <>
2018-11-16 14:51:34 +0800

    專案創建

---
+++ Assets/Main/Code/MainUICon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUICon : MonoBehaviour {
+	[Serializable]
+	public struct _UI {
+
+	}
+	public _UI UI;
+	
+	void Start () {
+			
+	}
+}
